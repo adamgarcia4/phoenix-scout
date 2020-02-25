afd44dd40ba704fa90acab8fb3c2ee19956c25b5
Adam Garcia <adameitangarcia@gmail.com>
2020-02-25 22:41:25 +0200

    Pulled Backend URL to env vars.

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -10,15 +10,9 @@ declare namespace NodeJS
     export interface ProcessEnv
     {
         NODE_ENV: 'development' | 'production' | 'test';
-        REACT_APP_FIREBASE_API_KEY: string
-        REACT_APP_FIREBASE_AUTH_DOMAIN: string
-        REACT_APP_FIREBASE_DATABASE_URL: string
-        REACT_APP_FIREBASE_PROJECT_ID: string
-        REACT_APP_FIREBASE_STORAGE_BUCKET: string
-        REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string
-        REACT_APP_FIREBASE_APP_ID: string
-        REACT_APP_FIREBASE_MEASUREMENT_ID: string,
         REACT_APP_TBA_KEY: string,
         REACT_APP_TBA_URL: string,
+        SERVER_URL: string,
+        MONGO_URL: string,
     }
 }
